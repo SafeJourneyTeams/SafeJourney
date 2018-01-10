37db65f1ce39848da9be4fddb803c5a6e8fb0826
Karma Samdrup <kasamdrup1995@gmail.com>
2018-01-10 09:46:53 +0600

    Layout is designed

---
+++ app/src/main/java/rsta/safejourney/FirstAidBlankFragment.java
@@ -0,0 +1,29 @@
+package rsta.safejourney;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstAidBlankFragment extends Fragment {
+
+
+    public FirstAidBlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_aid_blank, container, false);
+    }
+
+}
---
+++ app/src/main/java/rsta/safejourney/tabsPager.java
@@ -0,0 +1,67 @@
+package rsta.safejourney;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by user on 1/9/2018.
+ */
+
+public class tabsPager extends FragmentStatePagerAdapter {
+
+    String[] titles = new String[] {"Home", "Complaint", "Safety Tip", "First Aid"};
+
+    public tabsPager(FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+
+            case 0:
+                HomeBlankFragment blankFragment = new HomeBlankFragment();
+                return blankFragment;
+
+            case 1:
+                ComplaintBlankFragment blank1Fragment = new ComplaintBlankFragment();
+                return blank1Fragment;
+
+            case 2:
+                SafetyTipsBlankFragment blank2Fragment = new SafetyTipsBlankFragment();
+                return blank2Fragment;
+
+            case 3:
+                FirstAidBlankFragment blank3Fragment = new FirstAidBlankFragment();
+                return blank3Fragment;
+
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+
+        return 5;
+    }
+
+    @Override
+    public CharSequence getPageTitle (int position){
+        switch (position) {
+            case 0:
+                return "Home";
+            case 1:
+                return "Complaint";
+            case 2:
+                return "Safety Tips";
+            case 3:
+                return "First Aids";
+            default:
+                return null;
+        }
+    }
+}
